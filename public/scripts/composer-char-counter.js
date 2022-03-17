9336f0bdf5feced1408fbe3fb31f731ed41f0e26
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-03-17 08:43:21 -0700

    check length of remaining char in js and make or delete class of outOfRange, so css can decide color base on that

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,20 @@
 
 $(document).ready(function() {
   console.log('🟢 Document is loaded')
+
+  // get length of value in textarea after user input anything
   $('#tweet-text').on('input', function(e) {
     const maxLen = 140;
     const inputLen = $(this).val().length;
+    const remainLen = maxLen - inputLen;
+    // travel domTree to find counter and update it!
     const counter = $(this).parent().find("output.counter");
-    counter[0].innerText = maxLen - inputLen;
+    counter[0].innerText = remainLen;
+    // make or remove out-of-range class base on remaining length
+    if (remainLen < 0) {
+      $(counter).addClass('out-of-range')
+    } else {
+      $(counter).removeClass('out-of-range')
+    }
   });
 });
